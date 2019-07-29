1364768aedf142ea0dbc3f1954453296584b1233
Jan Johansson <jan.johansson@anticimexforsakringar.onmicrosoft.com>
2019-07-29 09:48:30 +0200

    Create project and first kata.

---
+++ Codewars/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars
+{
+    public class Kata
+    {
+        public static int SquareEveryDigit(int n)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Codewars/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Codewars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digits = 0;
+            Kata.SquareEveryDigit(digits);
+        }
+    }
+}
